8270e78546a4fac5131f921019c79eb1b12b0281
Çağla <cagla@Caglas-MacBook-Air.local>
2023-12-18 12:43:56 +0300

    Change directory structue

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-python3 project/manage.py makemigrations
+python3 manage.py makemigrations
 if [ $? -ne 0 ]; then
     echo "Error: makemigrations failed"
     exit 1
 else
-    python3 project/manage.py migrate
+    python3 manage.py migrate
     echo "Ayağa kalkıyorue"
-    python3 project/manage.py runserver 0.0.0.0:8000
+    python3 manage.py runserver 0.0.0.0:8000
 fi
